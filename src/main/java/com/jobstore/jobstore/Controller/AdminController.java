e527495c4085585ac51dfc7402a87a5fe0ca9762
raddadda <cyc123m@gmail.com>
2023-11-14 15:57:21 +0900

    contorller

---
+++ src/main/java/com/jobstore/jobstore/Controller/AdminController.java
@@ -0,0 +1,33 @@
+package com.jobstore.jobstore.Controller;
+
+import com.jobstore.jobstore.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AdminController {
+
+    @Autowired AdminService adminService;
+    //회원가입
+    @GetMapping("/admin/join")
+    public String joinAdmin(Model model){
+        model.addAttribute("adminDto",new adminDto());
+        return "adminJoin";
+    }
+    @PostMapping("/admin/join")
+    public String createAdmin(AdminDto adminDto){
+        //adminService.join(adminDto);
+        return "redirect:/";
+    }
+
+    //로그인
+    @GetMapping("/admin/login")
+    public String loginAdmin(Model model){
+       // model.addAttribute("loginDto",new loginDto());
+        return "adminJoin";
+    }
+
+}
