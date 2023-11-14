0e2f7e6e58e22c846c9709395f2e84cde0db4d24
raddadda <cyc123m@gmail.com>
2023-11-14 20:28:07 +0900

    spring security

---
+++ src/main/java/com/jobstore/jobstore/service/AdminDetailsService.java
@@ -0,0 +1,23 @@
+package com.jobstore.jobstore.service;
+
+import com.jobstore.jobstore.entity.Admin;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminDetailsService  implements UserDetailsService {
+    private final AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String adminid) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByAdminid(adminid)
+                .orElseThrow(() -> {
+                    return new UsernameNotFoundException("해당 유저를 찾을 수 없습니다.");
+                });
+        return new AdminService(admin);
+    }
+}
--- src/main/java/com/jobstore/jobstore/service/AdminService.java
+++ src/main/java/com/jobstore/jobstore/service/AdminService.java
@@ -1,7 +1,66 @@
 package com.jobstore.jobstore.service;
 
+import com.jobstore.jobstore.entity.Admin;
+import com.jobstore.jobstore.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Service
-public class AdminService {
+@Getter
+@Setter
+public class AdminService implements UserDetails {
+    private Admin admin;
+    public AdminService(Admin admin) {
+        this.admin = admin;
+    }
+
+    //해당 유저의 권한 목록
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collector = new ArrayList<>();
+        collector.add(() -> { return admin.getRole();}); // 람다식
+
+        return collector;
+    }
+
+    //비밀번호
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+    //아이디
+    @Override
+    public String getUsername() {
+        return admin.getAdminid();
+    }
+    //계정 만료 여부 true: 만료 안됨, false : 만료
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    //계정 잠김 여부 true: 잠기지 않음
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    //비밀번호 만료 여부 true: 만료 안됨
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    //사용자 활성화 여부 true: 활성화
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
 }
