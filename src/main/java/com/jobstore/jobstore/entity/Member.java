69d569740c98532c15c19e79d66304ccca42e3c9
kangseokjooo <zpckq69@naver.com>
2023-11-15 17:03:43 +0900

    Import error

--- src/main/java/com/jobstore/jobstore/entity/Member.java
+++ src/main/java/com/jobstore/jobstore/entity/Member.java
@@ -1,10 +1,7 @@
 package com.jobstore.jobstore.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 
