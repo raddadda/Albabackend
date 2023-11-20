866c96bcc6e5a46e1eae7c918f659f8b6777c447
justin-jig <justin-j@naver.com>
2023-11-20 19:54:05 +0900

    test

--- src/main/java/com/jobstore/jobstore/config/AwsConfig.java
+++ src/main/java/com/jobstore/jobstore/config/AwsConfig.java
@@ -4,10 +4,13 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AwsConfig {
 
     @Value("${cloud.aws.credentials.accessKey}")
@@ -20,11 +23,11 @@ public class AwsConfig {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
 
